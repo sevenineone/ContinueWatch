92aa0e55031e4dbd30a02564d546b3cf68e74680
sevenineone <alexeyka791@gmail.com>
2021-10-17 19:15:03 +0300

    lab2 done!

---
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -0,0 +1,44 @@
+package ru.spbstu.icc.kspt.lab2.continuewatch
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private var secondsElapsed: Int = 0
+    private lateinit var textSecondsElapsed: TextView
+
+
+    private var backgroundThread = Thread {
+        while (true) {
+            textSecondsElapsed.post {
+                textSecondsElapsed.text = getString(R.string.text, secondsElapsed++)
+            }
+            Thread.sleep(1000)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putInt(SECONDS, secondsElapsed)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
+        backgroundThread.start()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        secondsElapsed = savedInstanceState.getInt(SECONDS)
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    companion object {
+        const val SECONDS = "Seconds elapsed"
+    }
+
+}
---
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity2.kt
@@ -0,0 +1,54 @@
+package ru.spbstu.icc.kspt.lab2.continuewatch
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+
+class MainActivity2 : AppCompatActivity() {
+    private var secondsElapsed: Int = 0
+    private lateinit var textSecondsElapsed: TextView
+    private lateinit var sharedPrefs: SharedPreferences
+
+    private var backgroundThread = Thread {
+        while (true) {
+            if (this.lifecycle.currentState == Lifecycle.State.RESUMED) {
+                textSecondsElapsed.post {
+                    textSecondsElapsed.text = getString(R.string.text, secondsElapsed++)
+                }
+                Thread.sleep(1000)
+            }
+        }
+    }
+
+    init {
+        backgroundThread.start()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedPrefs = getPreferences(Context.MODE_PRIVATE)
+        setContentView(R.layout.activity_main)
+        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        secondsElapsed = sharedPrefs.getInt(SECONDS, 0)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        with(sharedPrefs.edit()) {
+            putInt(SECONDS, secondsElapsed)
+            apply()
+        }
+    }
+
+    companion object {
+        const val SECONDS = "Seconds elapsed"
+    }
+
+}
